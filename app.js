abc918810d09a971056c1fcc77f77097d858d4cc
Pedro Henrique <pedroschleder@outlook.com>
2017-09-03 17:25:23 -0300

    testing js imports

---
+++ app.js
@@ -0,0 +1,5 @@
+let monthForm = document.querySelector("#days-form")
+
+let opt = '1 <select><option value="1">One</option><option value="2">Two</option></select><br>';
+opt += '2 <select><option value="1">One</option><option value="2">Two</option></select>';
+monthForm.innerHTML = opt
